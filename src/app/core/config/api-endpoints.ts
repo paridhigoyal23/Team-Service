610bee91d81fec056e84a67192efe387aca46e75
Paridhi <paridhigoyal23@gmail.com>
2023-08-18 23:28:05 +0530

    Update trainingdatas

--- src/app/core/config/api-endpoints.ts
+++ src/app/core/config/api-endpoints.ts
@@ -4,4 +4,5 @@ export class ApiEndpoints {
     public static readonly USERS = `${this.BASE_URL}/users`;
     public static readonly EMPLOYEEDATA = `${this.BASE_URL}/employeesData`;
     public static readonly EMPLOYEEATTENDENCES = `${this.BASE_URL}/employeeAttendances`;
+    public static readonly TrainingData=`${this.BASE_URL}/trainingData`
 }
--- src/app/service/training-api-service.service.ts
+++ src/app/service/training-api-service.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from 'rxjs';
+import { ApiEndpoints } from '../core/config/api-endpoints';
 @Injectable({
   providedIn: 'root'
 })
@@ -8,13 +9,13 @@ export class TrainingApiServiceService {
 
   constructor(private http:HttpClient) { }
   getTrainingData():Observable<any[]>{
-    return this.http.get<any[]>('http://localhost:3000/trainingData')
+    return this.http.get<any[]>(ApiEndpoints.TrainingData)
   }
   saveTrainingData(data: any) {
     console.log("trainingData", data);
-    return this.http.post('http://localhost:3000/trainingData', data);
+    return this.http.post(ApiEndpoints.TrainingData, data);
 }
 updateTraining(data:any):Observable<any>{
-return this.http.put<any>(`${'http://localhost:3000/trainingData'}/${data.id}`,data)
+return this.http.put<any>(`${ApiEndpoints.TrainingData}/${data.id}`,data)
 }
 }
